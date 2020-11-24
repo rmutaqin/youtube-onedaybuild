da1a76b3e1bc33f238b749300f3ce60972608e15
Reza Mutaqin <rezamutaqin@Rezas-MacBook-Air.local>
2020-11-24 10:34:01 +0700

    Reza Description

--- youtube-onedaybuild/ViewController.swift
+++ youtube-onedaybuild/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        var i = 0
+        let tipe = "Ganteng"
+        let name = "Reza \(tipe)."
     }
 
 
