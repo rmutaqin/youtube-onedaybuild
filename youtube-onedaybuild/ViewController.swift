ab09a2188d9afbc64f1c4fee16506c09f8796986
Reza Mutaqin <rezamutaqin@Rezas-MacBook-Air.local>
2020-11-24 10:01:11 +0700

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Reza Mutaqin on 24/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
